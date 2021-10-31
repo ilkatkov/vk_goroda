ba0fa67daece1b20cacd8805333c5f49728b56a7
Ilya Katkov <55957279+ilkatkov@users.noreply.github.com>
2021-10-31 17:17:21 +0300

    Update add_city.py

--- add_city.py
+++ add_city.py
@@ -11,18 +11,3 @@ cursor.execute(query)
 print("Gorod dobavlen!")
 
 conn.commit()
-=======
-import sqlite3
-
-# инициализация 
-conn = sqlite3.connect("db.sqlite")
-cursor = conn.cursor()
-
-s = input("Vvedite gorod: ").encode('utf8','surrogateescape').decode('utf8','surrogateescape')
-
-query = "INSERT or REPLACE INTO cities VALUES ('{0}')".format(s)
-cursor.execute(query)
-print("Gorod dobavlen!")
-
-conn.commit()
-
