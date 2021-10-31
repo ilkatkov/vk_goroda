6590b2becc9198d31759e9bb93841baa60c2007e
Ilya Katkov <55957279+ilkatkov@users.noreply.github.com>
2021-10-31 17:20:16 +0300

    Update check_players.py

--- check_players.py
+++ check_players.py
@@ -10,15 +10,3 @@ l = sorted(l, key = lambda points: int(points[3]), reverse=True)
 
 for i in l:
     print(i)
-=======
-import sqlite3
-
-# инициализация db.sqlite
-conn = sqlite3.connect("db.sqlite")
-cursor = conn.cursor()
-query = "SELECT * FROM users"
-cursor.execute(query)
-l = cursor.fetchall()
-l = sorted(l, key = lambda points: int(points[3]), reverse=True)
-for i in l:
-    print(i)
